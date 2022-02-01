d2afa750900580306bb00e40bf67b20154fa9047
sam <sam@slatemane.com>
2022-01-31 19:29:46 -0500

    Assingment 3

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
+import About from "./AboutComponent";
 import { COMMENTS } from "../shared/comments";
 import { PARTNERS } from "../shared/partners";
 import { PROMOTIONS } from "../shared/promotions";
@@ -63,6 +64,11 @@ class Main extends Component {
             render={() => <Directory campsites={this.state.campsites} />}
           />
           <Route exact path="/contactus" component={Contact} />
+          <Route
+            exact
+            path="/AboutComponent"
+            render={() => <About partners={this.state.partners} />}
+          />
           <Route path="/directory/:campsiteId" component={CampsiteWithId} />
           <Redirect to="/home" />
         </Switch>
